8133c1afedb3a3cd739c71c8c6d69e870d7c6adf
kellstrum <kylekjellstrom@gmail.com>
2025-07-22 20:21:35 -0400

    Initial commit

--- Tutorials/dice.py
+++ Tutorials/dice.py
@@ -1,9 +1,13 @@
 import random
 
-number = random.randint(1, 6)
-roll = input("Type roll to roll the dice: ")
-if roll == "roll":
-    print("You rolled a", number)
-else:
-    print("You didn't type roll")
-    
+while True:
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    roll = input("Roll the dice? Y/N: ").capitalize()
+    if roll == "Y":
+        print(f"You rolled {die1}, {die2}")
+    elif roll == "N":
+        print("Goodbye!")
+        break
+    else:
+        print("That's not a valid option")
